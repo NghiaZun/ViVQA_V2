7399f0cc45d24fe9878514ecb880b332eccbd0d7
nghia-duong <nghia.duong@mservice.com.vn>
2025-12-16 07:59:20 +0700

    test

--- train_optimal.py
+++ train_optimal.py
@@ -475,7 +475,7 @@ def train():
     # =====================
     # RESUME FROM CHECKPOINT
     # =====================
-    resume_checkpoint = "/kaggle/input/e27/transformers/default/1/latest_checkpoint_optimal.pt"
+    resume_checkpoint = "/kaggle/input/e50/transformers/default/1/latest_checkpoint_optimal.pt"
     if os.path.exists(resume_checkpoint):
         print(f"\n{'='*70}")
         print(f"🔄 RESUMING FROM CHECKPOINT: {resume_checkpoint}")
